00989510506d6111fa772f904f144cfbe33f094e
Absydion <105251850+Absydion@users.noreply.github.com>
2023-03-22 21:47:49 +0100

    Entities added

--- src/main/java/com/app/entity/Flight.java
+++ src/main/java/com/app/entity/Flight.java
@@ -1,57 +1,38 @@
 package com.app.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String Flightnumber;
-    public String getFlightnumber() {
-        return Flightnumber;
-    }
-    public void setFlightnumber(String newFlightnumber) {
-        this.Flightnumber = newFlightnumber;
-    }
+
     private String Departueplace;
-    public String getDepartueplace() {
-        return Departueplace;
-    }
-    public void setDepartueplace(String newDepartueplace) {
-        this.Departueplace = newDepartueplace;
-    }
+
     private String Arrivalplace;
-    public String getArrivalplace() {
-        return Arrivalplace;
-    }
-    public void setArrivalplace(String newArrivalplace) {
-        this.Arrivalplace = newArrivalplace;
-    }
+
     private LocalDate Departuedate;
-    public LocalDate getDeparuedate() {
-        return Departuedate;
-    }
-    public void setDepartuedate(LocalDate newDepartuedate) {
-        this.Departuedate = newDepartuedate;
-    }
 
     private LocalTime Arrivaltime;
 
-    public LocalTime getArrivaltime() {
-        return Arrivaltime;
-    }
-public void setArrivaltime(LocalTime newArrivaltime) {
-        this.Arrivaltime = newArrivaltime;
-}
     private String Flightstatus;
-    public String getFlightstatus() {
-        return Flightstatus;
-    }
-    public void setFlightstatus(String newFlightstatus) {
-        this.Flightstatus = newFlightstatus;
-    }
-
 
 }
